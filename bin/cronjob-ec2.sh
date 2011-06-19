99d896df5b38328006bb0555169f763081f366a8
Uwe <uwe@uwevoelker.de>
2011-06-19 15:43:31 +0200

    cronjobs

--- bin/cronjob-ec2.sh
+++ bin/cronjob-ec2.sh
@@ -4,6 +4,6 @@ source /home/ec2-user/perl5/perlbrew/etc/bashrc
 
 cd /home/ec2-user/mittag
 perl bin/download.pl
-tar -czf `date +%Y-%m-%d`.tgz data
+tar -czf `date +%Y-%m-%d-%H-%M`.tgz data
 perl bin/extract.pl
 
---
+++ bin/cronjob-mail-ec2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /home/ec2-user/perl5/perlbrew/etc/bashrc
+
+cd /home/ec2-user/mittag
+perl bin/mail-download.pl
+tar -czf mail-`date +%Y-%m-%d-%H-%M`.tgz mail
+perl bin/mail-extract.pl
+
