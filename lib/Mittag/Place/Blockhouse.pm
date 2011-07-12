3510d72faf7c6376655e40823652305f004d8964
Uwe <uwe@uwevoelker.de>
2011-07-12 20:24:28 +0200

    fixed Blockhouse

--- lib/Mittag/Place/Blockhouse.pm
+++ lib/Mittag/Place/Blockhouse.pm
@@ -4,11 +4,13 @@ use utf8;
 use strict;
 use warnings;
 
+use HTML::TableExtract;
+
 use base qw/Mittag::Place/;
 
 
 sub id      { 1 }
-sub file    { 'block-house.txt' }
+sub file    { 'block-house.html' }
 sub name    { 'Block House' }
 sub type    { 'web' }
 sub address { 'Gänsemarkt Passage, 20354 Hamburg' }
@@ -23,44 +25,46 @@ sub download {
 
     my $url = 'http://www.block-house.de/block-house-restaurant-best-steaks-since-1968/block-house-qualitaet-ist-unsere-leidenschaft/block-house-lunch-time/'; # SEO gone mad :)
 
-    my $file = $self->file; $file =~ s/\.txt$/.html/;
-    $downloader->get_store($url, $file);
-
-    my $txt = $downloader->html2txt($file);
-
-    # remove empty lines
-    $txt =~ s/\n{2,}/\n/gms;
-
-    $downloader->store($txt, $self->file);
+    $downloader->get_store($url, $self->file);
 }
 
 sub extract {
     my ($self, $data, $importer) = @_;
 
-    my @data = grep { $_ } split /\n/, $data;
+    my $te = HTML::TableExtract->new;
+    $te->parse($data);
 
-    # two weeks
-    $self->_extract_week(\@data, $importer);
-    $self->_extract_week(\@data, $importer);
+    my $table = $te->first_table_found or die 'no table found';
+
+    my (@week1, @week2);
+    foreach my $row ($table->rows) {
+        push @week1, [$self->_trim_split($row->[0])];
+        push @week2, [$self->_trim_split($row->[1])];
+    }
+
+    $self->_extract_week(\@week1, $importer);
+    $self->_extract_week(\@week2, $importer);
 }
 
 sub _extract_week {
     my ($self, $data, $importer) = @_;
 
     # weekly offers
-    my ($price) = $self->_find(qr/^Wählen Sie täglich zwischen unseren Wochengerichten für (\d+,\d\d) €$/, $data);
+    my ($price) = $self->_find(qr/^Wählen Sie täglich zwischen unseren Wochengerichten für (\d+,\d\d) €$/, shift @$data);
     $price =~ s/,/./;
 
     # date is still missing ...
     my @weekly = ();
     foreach (1 .. 3) {
-        my $meal = join(' ', splice @$data, 0, 2);
+        my $meal = join ' ', @{shift @$data};
         push @weekly, [$meal, $price];
     }
 
     # daily offer
     foreach my $weekday (@weekdays) {
-        my $line = shift @$data;
+        my @day = @{shift @$data};
+
+        my $line = shift @day;
         unless ($line =~ /^$weekday \| (\d\d)\.(\d\d)\.(\d{4})$/) {
             $self->abort("date not found: $line");
         }
@@ -79,25 +83,17 @@ sub _extract_week {
             }
         }
 
-        my $meal = shift @$data;
+        next if $day[0] eq '— Himmelfahrt —';
+        next if $day[0] eq '— Pfingstmontag —';
 
-        next if $meal eq '— Himmelfahrt —';
-        next if $meal eq '— Pfingstmontag —';
-
-        $line = shift @$data;
-        unless ($line =~ /^(\d+,\d\d) €$/) {
-            $self->abort("price not found: $line");
+        # price
+        unless ($day[1] =~ /^(\d+,\d\d) €$/) {
+            $self->abort("price not found: $day[1]");
         }
         my $price = $1;
         $price =~ s/,/./;
 
-        unless (
-            $data->[0] =~ /^(Montag|Dienstag|Mittwoch|Donnerstag|Freitag) \| (\d\d)\.(\d\d)\.(\d{4})$/ or
-            $data->[0] =~ /^Wählen Sie täglich zwischen unseren Wochengerichten für (\d+,\d\d) €$/ or
-            $data->[0] eq 'Hauptspeisekarte'
-            ) {
-            $meal .= ' ' . shift @$data;
-        }
+        my $meal = join(' ', $day[0], $day[2]);
 
         $importer->save(
             id    => $self->id,
