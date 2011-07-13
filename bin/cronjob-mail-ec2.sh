36c400c8849fe63773cc157b015e1954ba54b4df
Uwe <uwe@uwevoelker.de>
2011-07-13 04:56:47 +0200

    cronjobs: move archive to backup folder

--- bin/cronjob-mail-ec2.sh
+++ bin/cronjob-mail-ec2.sh
@@ -4,6 +4,6 @@ source /home/ec2-user/perl5/perlbrew/etc/bashrc
 
 cd /home/ec2-user/mittag
 perl bin/mail-download.pl
-tar -czf mail-`date +%Y-%m-%d-%H-%M`.tgz mail
+tar -czf backup/mail-`date +%Y-%m-%d-%H-%M`.tgz mail
 perl bin/mail-extract.pl
 
