eadf26c0a814ed24a1c9b2ae424dc25a0769f16c
Uwe <uwe@uwevoelker.de>
2011-07-21 20:29:27 +0200

    empty download directories

--- bin/cronjob-ec2.sh
+++ bin/cronjob-ec2.sh
@@ -3,6 +3,7 @@
 source /home/ec2-user/perl5/perlbrew/etc/bashrc
 
 cd /home/ec2-user/mittag
+rm -f data/*
 perl bin/download.pl
 tar -czf backup/`date +%Y-%m-%d-%H-%M`.tgz data
 perl bin/extract.pl
--- bin/cronjob-mail-ec2.sh
+++ bin/cronjob-mail-ec2.sh
@@ -3,6 +3,7 @@
 source /home/ec2-user/perl5/perlbrew/etc/bashrc
 
 cd /home/ec2-user/mittag
+rm -f mail/*.txt
 perl bin/mail-download.pl
 tar -czf backup/mail-`date +%Y-%m-%d-%H-%M`.tgz mail
 perl bin/mail-extract.pl
