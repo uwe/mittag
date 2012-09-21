e70d6e043ddb5c12462e9783d5954ec69728bbdc
Ivan Fomichev <ifomichev@gmail.com>
2012-09-21 21:19:15 +0200

    Fixed Opera

--- lib/Mittag/Place/Opera.pm
+++ lib/Mittag/Place/Opera.pm
@@ -42,6 +42,7 @@ sub extract {
     my $date = sprintf('%d-%02d-%02d', $year, $self->_from_month($month), $day);
 
     my $data = (($mail->parts)[0]->parts)[0]->body_str;
+    return if $data =~ /^Beschreibung/; # old format
     $data =~ s/\r//g;
     $data =~ s/…/.../g;
     my @data = $self->_trim_split($data);
@@ -57,24 +58,21 @@ sub extract {
 
     while (@data) {
         my $line = shift @data;
+        next if $line !~ /\w/;
         last if $line =~ /^Kaffee und Espresso/;
         last if $line =~ /^Halbe Portion Dessert/;
 
         if ($line !~ /€/) {
-            # missing Euro sign?
-            unless ($line =~ s/\.\.\s*(\d+,\d\d)$/..€$1/) {
-                # run away line?
-                if ($data[0] =~ /€/) {
-                    $line .= ' ' . shift @data;
-                } else {
-                    $self->abort('Two lines without Euro sign');
-                }
+            # run away line?
+            if ($data[0] =~ /€/) {
+                $line .= ' ' . shift @data;
+            } else {
+                $self->abort('Two lines without Euro sign');
             }
         }
 
-        my @meal  = split /\s*\.{2,}\s*/, $line;
-        my $price = pop @meal;
-        $price =~ s/€//;
+        my ($meal, $price) = split /€/, $line;
+
         $price =~ s/\s//g;
         $price =~ s/,/./;
         # remove comments
@@ -83,13 +81,6 @@ sub extract {
         unless ($price =~ /^\d{1,2}\.\d{2}$/) {
             $self->abort("Unknown price '$price'");
         }
-
-        if (@meal > 2) {
-            $self->abort("Meal too long: '@meal'");
-        }
-
-        my $meal = shift @meal;
-        $meal .= ' ('.$meal[0].')' if @meal;
 
         $importer->save(
             id    => $self->id,
