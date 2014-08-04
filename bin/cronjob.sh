17f9253f219a679c6d4f3260302b76adf63e6e47
Uwe Voelker <uwe@uwevoelker.de>
2014-08-04 19:40:38 +0200

    fix cronjob

--- bin/cronjob.sh
+++ bin/cronjob.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-export LANG='de_DE.utf8'
-
-cd /home/uwe/mittag
+cd /home/app
 rm -f data/*
-/opt/perl/bin/perl bin/download.pl
-#tar -czf backup/`date +%Y-%m-%d-%H-%M`.tgz data
-/opt/perl/bin/perl bin/extract.pl
-
+perl bin/download.pl
+perl bin/extract.pl
